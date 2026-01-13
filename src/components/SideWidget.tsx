a81d790d77c4c2421c710c5187cebb1d6cd5ab80
Vivek Joshi <vj69376@gmail.com>
2026-01-13 13:13:33 +0530

    Inital commit

--- src/components/SideWidget.tsx
+++ src/components/SideWidget.tsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
+
 export default function SideWidget() {
   useEffect(() => {
   }, []);
 
+  const WidgetComponent = 'react-widget-uv' as any;
+
   return (
-    <react-widget-uv
+    <WidgetComponent
       agent_id="60066779-273a-4dc8-9d1e-ae0aa6c0108e"
       schema="237bc2c6-ef87-4def-be84-701cd673df42"
       type="customwidget"
